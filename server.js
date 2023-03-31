dd0620e6adb8dfa046d688bfcb5ed005dd890b0a
Owall <97934878+MayOwall@users.noreply.github.com>
2023-03-31 17:32:36 +0900

    feat : /signup POST 통신 로직 구현

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
+require("dotenv/config");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { PORT, MONGODB_ID, MONGODB_PASSWORD } = process.env;
+
+// MongoDB
+const mongoClient = require("mongodb").MongoClient;
 const express = require("express");
 const app = express();
-const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+app.use(bodyParser.json());
+
+let db;
+
+mongoClient.connect(
+  `mongodb+srv://${MONGODB_ID}:${MONGODB_PASSWORD}@cluster0.sjgsexl.mongodb.net/bookstack?retryWrites=true&w=majority`,
+  (err, client) => {
+    if (err) throw err;
+    db = client.db("bookstack");
+
+    app.listen(PORT, () => {
+      console.log("📡 BookStack BE is working");
+    });
+  }
+);
+
+app.route("/").get((req, res) => res.send("Hello, Bookstack 📚"));
+
+app.route("/signup").get((req, res) => {
+  res.send("signup");
 });
 
-app.listen(port, () => {
-  console.log(`📡 BookStack BE Server is Working`);
+app.route("/signup").post((req, res) => {
+  const { id, pw, pwConfirm, nickname } = req.body;
+  res.send("signup");
 });
