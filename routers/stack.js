ae9a14b6ea02f9f13ff9b5801217a3c9d18237f5
Owall <97934878+MayOwall@users.noreply.github.com>
2023-04-14 14:03:36 +0900

    feat : /stack/create POST API 구현

--- routers/stack.js
+++ routers/stack.js
@@ -35,4 +35,41 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post("/create", async (req, res) => {
+  try {
+    const token = req.header("authorization");
+    const { _id } = jwt.verify(token, JWT_SECRET_KEY);
+    const { db } = req.app;
+
+    const { title, author, publisher, date, detail } = req.body;
+    const user = await db
+      .collection("login")
+      .findOne({ _id })
+      .catch(() => res.status(200).json({ error: "no such id" }));
+    const { posts, bookIdCount } = user;
+    const nextData = {
+      no: bookIdCount + 1,
+      title,
+      author,
+      publisher,
+      date,
+      detail,
+    };
+    posts[posts.length - 1].stackList.push(nextData);
+    user.bookCount += 1;
+    user.bookIdCount += 1;
+
+    await db.collection("login").updateOne({ _id }, { $set: user });
+    res.json({ result: "success" });
+  } catch (err) {
+    console.log(err);
+    if (err.message === "jwt expired") {
+      res.status(200).json({ error: "Token Expired" });
+    } else if (err.message === "invalid token") {
+      res.status(200).json({ error: "Invalid Token" });
+    } else {
+      res.status(500).json({ result: "server error" });
+    }
+  }
+});
 module.exports = router;
